bfabe42c38ccf5d781e9020711f2a6f92292f70a
Gabriel <gabrielfreitasgarcia@globo.com>
2021-01-28 22:54:46 -0300

    Movimentação do primeiro bloco

---
+++ MeuTetris/Assets/Scripts/Control.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public static int height = 20;
+    public static int width = 10;
+    public static Transform[,] table = new Transform[width, height];
+
+    public bool insideBorders(Moving blocks)
+    {
+        foreach (Transform block in blocks.transform) {
+            if (block.transform.position.y < 0) {
+                blocks.setMoving(false);
+                return false;
+            } else if (block.transform.position.x < 0)
+            {
+                return false;
+            } else if (block.transform.position.x > width)
+            {
+                return false;
+            } else if (block.transform.position.y < height && block.transform.position.y >= 0)
+            {
+                if (table[Mathf.RoundToInt(block.transform.position.x), Mathf.RoundToInt(block.transform.position.y)] != null)
+                {
+                    if (table[Mathf.RoundToInt(block.transform.position.x), Mathf.RoundToInt(block.transform.position.y)] != block)
+                    {
+                        blocks.setMoving(false);
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ MeuTetris/Assets/Scripts/Moving.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour{
+
+    private float counter = 0;
+    private int speed = 2;
+    private Vector3 velocity = new Vector3(0, 0, 0);
+    private bool moving = true;
+    private int heigth = 20;
+    private int width = 10;
+    private Control con;
+    // Start is called before the first frame update
+    void Start(){
+        counter = Time.time;
+        con = FindObjectOfType<Control>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        velocity = new Vector3(0, 0, 0);
+        if ((Time.time - counter) >= 1 && moving)
+        {
+            velocity.y = -speed;
+            counter = Time.time;
+        }
+        transform.position += velocity;
+        
+        if (!con.insideBorders(this))
+        {
+            transform.position -= velocity;
+        }
+        else
+        {
+            Control.table[Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y)] = transform;
+            Control.table[Mathf.RoundToInt(transform.position.x - velocity.x), Mathf.RoundToInt(transform.position.y - velocity.y)] = null;
+        }
+    }
+    public void setMoving(bool active){
+        this.moving = active;
+    }
+    
+}
